f8dc21877f2894c241aae1c515ae887e0b2a02e8
rajat <grajat315@gmail.com>
2021-08-11 00:31:57 +0530

    Developed front for index

--- products/models.py
+++ products/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class products_model(models.Model):
-    PID = models.IntegerField()
+    PID = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
     tags = models.CharField(max_length=5100)
 
     def __str__(self):
-        return self.title
+        return self.name
