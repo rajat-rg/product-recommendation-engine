8cd55fc73c3b559fd47813160c0a0ec9cf64023f
rajat <grajat315@gmail.com>
2021-08-12 22:52:58 +0530

    added signup login

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name="Home"),
+    path('signup', views.handleSignup, name="Signup"),
+    path('login', views.handleLogin, name="Login"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
 # Create your views here.
 def index(request):
     context = {"search": ["like","like","like","like","like"], "like":["like","like","like","like","like"]}
@@ -9,3 +11,32 @@ def index(request):
 def recommend(request):
     context = {}
     return render(request, "recommend.html", context) 
+
+def handleSignup(request):
+    if request.method == "POST":
+        fname = request.POST.get("fname")
+        lname = request.POST.get("lname")
+        email = request.POST.get("email")
+        username = request.POST.get("username")
+        pass1 = request.POST.get("pass1")
+        pass2 = request.POST.get("pass2")
+
+        if pass1 == pass2:
+            user = User.objects.create_user(username, email, pass1, first_name = fname, last_name = lname)
+            user.save()
+        else:
+            messages.info(request, "Password didn't matched !!")
+
+    return redirect("Home")
+
+def handleLogin(request):
+    if request.method == "POST":
+        username = request.POST.get("loginusername")
+        password = request.POST.get("loginpassword")
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("Home")
+        else:
+            messages.warning(request, "Youre not recognized !!")
+    return render("index.html")
