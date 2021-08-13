db4d73a45765f6c4f0b7acc1f15071a2e33c39a6
rajat <grajat315@gmail.com>
2021-08-13 22:08:30 +0530

    working model is ready

--- products/views.py
+++ products/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import redirect, render
+from django.http import JsonResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from .models import products_model
 import pickle
 # Create your views here.
+
+products = products_model.objects.all()
+similarity = pickle.load(open('similarity.pkl','rb'))
+
 def index(request):
     context = {"search": ["like","like","like","like","like"], "like":["like","like","like","like","like"]}
     return render(request,"index.html", context)
@@ -12,24 +17,30 @@ def index(request):
 
 def recommendations(request):
     context = {}
-    if request.method == "POST":
+    if request.method == "GET":
 
-        searched_movie = request.POST.get("search")
-        print("\n\n\n\n ye search hua hai bhaiya",searched_movie)
-        if searched_movie == "":
-            print('!!!!!!!!!!!!!!!!!!!!!!')
+        if 'term' in request.GET:
+            qs = products_model.objects.filter(name__contains=request.GET.get('term'))
+            products = list()
+            for product in qs:
+                products.append(product.name)
+            return JsonResponse(products, safe=False)
+
+        searched_product = request.GET.get("search")
+
+        if searched_product == "":
             return redirect("Home")
-        product = products_model.objects.all().filter(name = searched_movie)[0]
+        product = products_model.objects.all().filter(name = searched_product)[0]
         product_index = product.id -1
-        similarity = pickle.load("5000_similarity")
-        distances = similarity[product_index]
+        searched_product = products_model.objects.get(id = product_index+1)
+        distances = similarity[product_index][1]
         product_list = sorted(list(enumerate(distances)), reverse= True, key = lambda x: x[1])[1:6]
         product_recommended = list()
         poster = list()
         for i in product_list:
             x =products_model.objects.get(id= i[0]+1)
             product_recommended.append(x)  
-        context = {"product": searched_movie,"recommendations": product_recommended}
+        context = {"product": searched_product,"recommendations": product_recommended}        
     return render(request, "recommend.html", context) 
 
 def handleSignup(request):
