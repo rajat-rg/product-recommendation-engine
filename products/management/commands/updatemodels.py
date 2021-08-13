c5ee94db635d4eaab64b666dee1d6ca07004cb98
rajat <grajat315@gmail.com>
2021-08-13 18:27:14 +0530

    model updated with images and url

--- products/management/commands/updatemodels.py
+++ products/management/commands/updatemodels.py
@@ -9,7 +9,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         data = pd.read_csv('5000_products.csv')
-        for PID, TITLE, TAGS in zip(data.uniq_id, data.product_name, data.tags):
-            model = products_model(PID = PID, name = TITLE, tags = TAGS)
+        for PID, TITLE, TAGS, URL, IMAGE in zip(data.uniq_id, data.product_name, data.tags, data.product_url, data.image):
+            model = products_model(PID = PID, name = TITLE, tags = TAGS, url = URL, image =IMAGE)
             model.save()
         # now run the command in cmd: python manage.py updatemodels 
--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from .models import products_model
+import pickle
 # Create your views here.
 def index(request):
     context = {"search": ["like","like","like","like","like"], "like":["like","like","like","like","like"]}
@@ -10,21 +11,25 @@ def index(request):
 
 
 def recommendations(request):
+    context = {}
     if request.method == "POST":
 
-        searched_movie = request.method.get("search")
-        product = products_model.objects.all().filter(title = searched_movie)[0]
-        product_index = product.id -1 
-        distances = 5000_similarity[product_index]
-        movie_list = sorted(list(enumerate(distances)), reverse= True, key = lambda x: x[1])[1:6]
+        searched_movie = request.POST.get("search")
+        print("\n\n\n\n ye search hua hai bhaiya",searched_movie)
+        if searched_movie == "":
+            print('!!!!!!!!!!!!!!!!!!!!!!')
+            return redirect("Home")
+        product = products_model.objects.all().filter(name = searched_movie)[0]
+        product_index = product.id -1
+        similarity = pickle.load("5000_similarity")
+        distances = similarity[product_index]
+        product_list = sorted(list(enumerate(distances)), reverse= True, key = lambda x: x[1])[1:6]
+        product_recommended = list()
         poster = list()
-        movie_recommended = list()
-        for i in movie_list:
-            x =movies_model.objects.get(id= i[0]+1)
-            poster.append(x.movie_id)
-            movie_recommended.append(x)
-        return poster, movie_recommended 
-        context = {"product": "name","recommendations": ["like","like","like","like","like"] }
+        for i in product_list:
+            x =products_model.objects.get(id= i[0]+1)
+            product_recommended.append(x)  
+        context = {"product": searched_movie,"recommendations": product_recommended}
     return render(request, "recommend.html", context) 
 
 def handleSignup(request):
